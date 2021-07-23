258264acc0f674fb1c466c721c1a5c27cfd392b6
Sharp0802 <nrs070802@gmail.com>
2021-07-24 04:57:06 +0900

    Add method for multi language support

---
+++ KakaotalkAdConcealer/Gui/LanguageExtension.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using KakaotalkAdConcealer.Properties;
+
+namespace KakaotalkAdConcealer.Gui
+{
+    public class LanguageExtension
+    {
+        private static CultureInfo[] Cache { get; set; }
+
+        public static CultureInfo[] GetAvailableCultures()
+        {
+            return Cache ??= GetAvailableCulturesWithoutCache().ToArray();
+            
+            static IEnumerable<CultureInfo> GetAvailableCulturesWithoutCache()
+            {
+                var manager = new ResourceManager(typeof(Resources));
+
+                foreach (var culture in CultureInfo.GetCultures(CultureTypes.AllCultures))
+                {
+                    bool supported;
+                    try
+                    {
+                        supported = manager.GetResourceSet(culture, true, false) is not null;
+                    }
+                    catch (CultureNotFoundException)
+                    {
+                        supported = false;
+                    }
+
+                    if (supported)
+                        yield return culture;
+                }
+            }
+        }
+    }
+}
