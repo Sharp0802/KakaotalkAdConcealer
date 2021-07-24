d3327ac930a597c7c982b6099931a9c481de733e
Sharp0802 <nrs070802@gmail.com>
2021-07-24 18:23:29 +0900

    Refactoring code to use Initializable`1

--- KakaotalkAdConcealer/App.cs
+++ KakaotalkAdConcealer/App.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Globalization;
 using System.Reflection;
 using System.Windows.Forms;
 using KakaotalkAdConcealer.Common;
@@ -11,51 +12,43 @@ namespace KakaotalkAdConcealer
 {
     public class App : IDisposable
     {
+        private static object LockHandle { get; } = new();
+        private static App Instance { get; set; }
+
         private AdBlockContext Context { get; } = new();
 
-        public NotifyIcon Startup()
+        private event Action<CultureInfo> CultureUpdated;
+
+        public static App Create()
         {
-            var menu = new ContextMenuBuilder()
-                .Add(out var title)
-                .AddSeparator()
-                .Add(out var startup)
-                .AddSeparator()
-                .Add(out var removeAll)
-                .Add(out var removeEmbedded)
-                .Add(out var removePopup)
-                .AddSeparator()
-                .Add(out var exit)
-                .Build();
-
-            title.Text = Resources.Title;
-            title.ForeColor = Color.Gray;
-            
-            startup.Text = Resources.ActivateAtStartup;
-            startup.Checked = StartupRegister.IsAssigned(Resources.AppName);
-            startup.Click += (_, _) => startup.Checked = StartupRegister.Toggle(Resources.AppName);
-
-            removeAll.Text = Resources.RemoveAllAds;
-            removeAll.Click += (_, _) => removeAll.Checked = Context.ToggleBlockState();
-            
-            removeEmbedded.Text = Resources.RemoveEmbeddedAdsOnce;
-            removeEmbedded.Click += (_, _) => _ = Context.BlockOnce(BlockType.Embedded);
-            
-            removePopup.Text = Resources.RemovePopupAdsOnce;
-            removePopup.Click += (_, _) => _ = Context.BlockOnce(BlockType.Popup);
-
-            exit.Text = Resources.Quit;
-            exit.Click += (_, _) =>
+            lock (LockHandle)
             {
-                Application.Exit();
-                Environment.Exit(0);
-            };
+                Instance = Instance is null
+                    ? new App()
+                    : throw new InvalidProgramException();
+                
+                Initializable<ToolStripMenuItem>.Initialize(Instance);
+                Instance.CultureUpdated?.Invoke(CultureInfo.InvariantCulture);
+                _ = new NotifyIcon
+                {
+                    Visible = true,
+                    Icon = Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location),
+                    ContextMenuStrip = new ContextMenuBuilder()
+                        .Add(Instance.Title.Value)
+                        .AddSeparator()
+                        .Add(Instance.Startup.Value)
+                        .Add(Instance.Language.Value)
+                        .AddSeparator()
+                        .Add(Instance.RemoveAll.Value)
+                        .Add(Instance.RemoveEmbedded.Value)
+                        .Add(Instance.RemovePopup.Value)
+                        .AddSeparator()
+                        .Add(Instance.Quit.Value)
+                        .Build()
+                };
 
-            return new NotifyIcon
-            {
-                Visible = true,
-                Icon = Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location),
-                ContextMenuStrip = menu
-            };
+                return Instance;
+            }
         }
 
         public void Dispose()
@@ -63,5 +56,63 @@ namespace KakaotalkAdConcealer
             Context.Dispose();
             GC.SuppressFinalize(this);
         }
+
+        private Initializable<ToolStripMenuItem> Title { get; } = new(title =>
+        {
+            Instance.CultureUpdated += _ => title.Text = Resources.Title;
+            title.ForeColor = Color.Gray;
+        });
+
+        private Initializable<ToolStripMenuItem> Startup { get; } = new(startup =>
+        {
+            Instance.CultureUpdated += _ => startup.Text = Resources.ActivateAtStartup;
+            startup.Checked = StartupRegister.IsAssigned(Resources.AppName);
+            startup.Click += (_, _) => startup.Checked = StartupRegister.Toggle(Resources.AppName);
+        });
+
+        private Initializable<ToolStripMenuItem> Language { get; } = new(language =>
+        {
+            Instance.CultureUpdated += _ =>
+                language.Text = (Resources.Culture ?? CultureInfo.InvariantCulture).ToISOCode();
+            var cultures = LanguageExtension.GetAvailableCultures();
+            foreach (var culture in cultures)
+            {
+                var item = new ToolStripMenuItem(culture.ToISOCode());
+                item.Click += (_, _) =>
+                {
+                    Resources.Culture = culture;
+                    Instance.CultureUpdated?.Invoke(culture);
+                };
+                language.DropDownItems.Add(item);
+            }
+        });
+
+        private Initializable<ToolStripMenuItem> RemoveAll { get; } = new(removeAll =>
+        {
+            Instance.CultureUpdated += _ => removeAll.Text = Resources.RemoveAllAds;
+            removeAll.Click += (_, _) => removeAll.Checked = Instance.Context.ToggleBlockState();
+        });
+
+        private Initializable<ToolStripMenuItem> RemoveEmbedded { get; } = new(removeEmbedded =>
+        {
+            Instance.CultureUpdated += _ => removeEmbedded.Text = Resources.RemoveEmbeddedAdsOnce;
+            removeEmbedded.Click += (_, _) => _ = Instance.Context.BlockOnce(BlockType.Embedded);
+        });
+
+        private Initializable<ToolStripMenuItem> RemovePopup { get; } = new(removePopup =>
+        {
+            Instance.CultureUpdated += _ => removePopup.Text = Resources.RemovePopupAdsOnce;
+            removePopup.Click += (_, _) => _ = Instance.Context.BlockOnce(BlockType.Popup);
+        });
+
+        private Initializable<ToolStripMenuItem> Quit { get; } = new(quit =>
+        {
+            Instance.CultureUpdated += _ => quit.Text = Resources.Quit;
+            quit.Click += (_, _) =>
+            {
+                Application.Exit();
+                Environment.Exit(0);
+            };
+        });
     }
 }
