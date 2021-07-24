e1743330eebe4480bfd398289bce570a28a95ac9
Sharp0802 <nrs070802@gmail.com>
2021-07-24 18:22:38 +0900

    Add class to initialize at outside of target class

---
+++ KakaotalkAdConcealer/Common/Initializable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace KakaotalkAdConcealer.Common
+{
+    public class Initializable<T> where T : new()
+    {
+        public static void Initialize(object instance)
+        {
+            foreach (var initializer in instance
+                .GetType()
+                .GetProperties(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public)
+                .Where(property => property.PropertyType == typeof(Initializable<T>))
+                .Select(property => property.GetMethod?.CreateDelegate<Func<Initializable<T>>>(instance)))
+            {
+                var initializable = initializer?.Invoke() 
+                                    ?? throw new NullReferenceException("Initializer is null");
+                initializable.Initializer.Invoke(initializable.Value);
+            }
+        }
+        
+        public Initializable(Action<T> initializer)
+        {
+            Value = new T();
+            Initializer = initializer;
+        }
+
+        public T Value { get; }
+        public Action<T> Initializer { get; }
+    }
+}
