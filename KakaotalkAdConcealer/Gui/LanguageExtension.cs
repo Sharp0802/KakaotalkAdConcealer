1ab1eefc1727fea3b13fd586b33167aa49edd81f
Sharp0802 <nrs070802@gmail.com>
2021-08-26 18:58:14 +0900

    Add comments to LanguageExtension

--- KakaotalkAdConcealer/Gui/LanguageExtension.cs
+++ KakaotalkAdConcealer/Gui/LanguageExtension.cs
@@ -1,15 +1,23 @@
-﻿using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
+﻿using System.Globalization;
 using System.Resources;
 using KakaotalkAdConcealer.Properties;
 
 namespace KakaotalkAdConcealer.Gui
 {
+    /// <summary>
+    /// Helper for localization
+    /// </summary>
     public static class LanguageExtension
     {
+        /// <summary>
+        /// Cache for supported culture info list
+        /// </summary>
         private static CultureInfo[] Cache { get; set; }
 
+        /// <summary>
+        /// Get supported culture info list
+        /// </summary>
+        /// <returns>Supported culture info list</returns>
         public static IEnumerable<CultureInfo> GetAvailableCultures()
         {
             return Cache ??= GetAvailableCulturesWithoutCache().ToArray();
@@ -35,6 +43,11 @@ namespace KakaotalkAdConcealer.Gui
             }
         }
 
+        /// <summary>
+        /// Get ISO 639-2 three-letter code for CultureInfo
+        /// </summary>
+        /// <param name="info"></param>
+        /// <returns></returns>
         public static string ToISOCode(this CultureInfo info)
         {
             return info.Equals(CultureInfo.InvariantCulture) 
