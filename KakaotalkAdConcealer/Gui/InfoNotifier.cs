8690632f89a306be874662658280c513a6dd46e1
Sharp0802 <nrs070802@gmail.com>
2021-08-26 22:19:29 +0900

    Add class to splash notification on startup

---
+++ KakaotalkAdConcealer/Gui/InfoNotifier.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace KakaotalkAdConcealer.Gui
+{
+	public class InfoNotifier
+	{
+		public static void ShowRunningNotification()
+		{
+			new ToastContentBuilder()
+				.AddText("KakaotalkAdConcealer", hintMaxLines: 1)
+				.AddText("Ads blocker is running now!")
+				.Show();
+		}
+	}
+}
