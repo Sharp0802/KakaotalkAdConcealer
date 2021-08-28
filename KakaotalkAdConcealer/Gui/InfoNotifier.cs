ff21ff3d16633b1c368244e73a5bb1c5323e3b99
Sharp0802 <nrs070802@gmail.com>
2021-08-28 13:42:49 +0900

    Add comments to InfoNotifier

--- KakaotalkAdConcealer/Gui/InfoNotifier.cs
+++ KakaotalkAdConcealer/Gui/InfoNotifier.cs
@@ -3,8 +3,14 @@ using Microsoft.Toolkit.Uwp.Notifications;
 
 namespace KakaotalkAdConcealer.Gui
 {
+	/// <summary>
+	/// Helper for showing toast notification
+	/// </summary>
 	public class InfoNotifier
 	{
+		/// <summary>
+		/// Show notification for startup
+		/// </summary>
 		public static void ShowRunningNotification()
 		{
 			new ToastContentBuilder()
