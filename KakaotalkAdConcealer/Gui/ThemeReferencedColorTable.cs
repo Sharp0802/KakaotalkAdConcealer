43e2d8d9e004c84547afffb7d79f0b2de64b2984
Sharp0802 <nrs070802@gmail.com>
2021-08-28 13:30:54 +0900

    Add color table class to make notifyicon context menu to use color by windows theme

---
+++ KakaotalkAdConcealer/Gui/ThemeReferencedColorTable.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+using Windows.UI.ViewManagement;
+
+namespace KakaotalkAdConcealer.Gui
+{
+	public class ThemeReferencedColorTable : ProfessionalColorTable
+	{
+		private static UISettings Settings { get; } = new UISettings();
+
+		private static Color Background
+		{
+			get
+			{
+				var color = Settings.GetColorValue(UIColorType.Background);
+				return Color.FromArgb(color.A, color.R, color.G, color.B);
+			}
+		}
+
+		private static Color Smoke
+		{
+			get
+			{
+				var color = Settings.GetColorValue(UIColorType.Foreground);
+				return Color.FromArgb(color.A, color.R, color.G, color.B);
+			}
+		}
+
+		public override Color MenuItemBorder => Smoke;
+
+		public override Color MenuItemSelected => Smoke;
+
+		public override Color ToolStripDropDownBackground => Background;
+
+		public override Color ImageMarginGradientBegin => Background;
+
+		public override Color ImageMarginGradientMiddle => Background;
+
+		public override Color ImageMarginGradientEnd => Background;
+	}
+}
