01cd672efc5796b8a13c5a4817381e61ab685b82
Sharp0802 <nrs070802@gmail.com>
2021-08-28 22:35:33 +0900

    Fix resources error

--- KakaotalkAdConcealer.Forms/Properties/Resources.Designer.cs
+++ KakaotalkAdConcealer.Forms/Properties/Resources.Designer.cs
@@ -9,6 +9,9 @@
 //------------------------------------------------------------------------------
 
 namespace KakaotalkAdConcealer.Forms.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -36,7 +39,7 @@ namespace KakaotalkAdConcealer.Forms.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("KakaotalkAdConcealer.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("KakaotalkAdConcealer.Forms.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
