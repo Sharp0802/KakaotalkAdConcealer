9fa1d299b5c9dd44829f4e636ed1153fdba86199
Sharp0802 <nrs070802@gmail.com>
2021-08-28 17:11:40 +0900

    Rename project KakaotalkAdConcealer to KakaotalkAdConcealer.Forms

---
+++ KakaotalkAdConcealer.Forms/Gui/InfoNotifier.cs
@@ -0,0 +1,22 @@
+﻿using KakaotalkAdConcealer.Properties;
+using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace KakaotalkAdConcealer.Gui
+{
+	/// <summary>
+	/// Helper for showing toast notification
+	/// </summary>
+	public class InfoNotifier
+	{
+		/// <summary>
+		/// Show notification for startup
+		/// </summary>
+		public static void ShowRunningNotification()
+		{
+			new ToastContentBuilder()
+				.AddText(Resources.GoodbyeAds, hintMaxLines: 1)
+				.AddText(Resources.RunningNow)
+				.Show();
+		}
+	}
+}
---
+++ KakaotalkAdConcealer.Forms/Gui/LanguageExtension.cs
@@ -0,0 +1,58 @@
+﻿using System.Globalization;
+using System.Resources;
+using KakaotalkAdConcealer.Properties;
+
+namespace KakaotalkAdConcealer.Gui
+{
+    /// <summary>
+    /// Helper for localization
+    /// </summary>
+    public static class LanguageExtension
+    {
+        /// <summary>
+        /// Cache for supported culture info list
+        /// </summary>
+        private static CultureInfo[] Cache { get; set; }
+
+        /// <summary>
+        /// Get supported culture info list
+        /// </summary>
+        /// <returns>Supported culture info list</returns>
+        public static IEnumerable<CultureInfo> GetAvailableCultures()
+        {
+            return Cache ??= GetAvailableCulturesWithoutCache().ToArray();
+            
+            static IEnumerable<CultureInfo> GetAvailableCulturesWithoutCache()
+            {
+                var manager = new ResourceManager(typeof(Resources));
+
+                foreach (var culture in CultureInfo.GetCultures(CultureTypes.AllCultures))
+                {
+                    bool supported;
+                    try
+                    {
+                        supported = manager.GetResourceSet(culture, true, false) is not null;
+                    }
+                    catch (CultureNotFoundException)
+                    {
+                        supported = false;
+                    }
+                    if (supported)
+                        yield return culture;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get ISO 639-2 three-letter code for CultureInfo
+        /// </summary>
+        /// <param name="info"></param>
+        /// <returns></returns>
+        public static string ToISOCode(this CultureInfo info)
+        {
+            return info.Equals(CultureInfo.InvariantCulture) 
+                ? "default (eng)" 
+                : info.ThreeLetterISOLanguageName;
+        }
+    }
+}
